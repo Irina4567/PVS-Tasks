4e27fa4dbf30ba8a61549e0a86ca90e0460556b8
Irina <irina.pavliuchenkova@yandex.ru>
2021-07-06 16:41:28 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+unsigned factorial(unsigned x)
+{
+  return x == 0 ? 1 : x * factorial(x - 1);
+}
+////////////////
+
+//////////////////////////
+
+//////////////////
+
+///
+
+///////

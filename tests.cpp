14f195c316350368bfdb52e3ced2bb9cc7eb10c0
Irina4567 <81246608+Irina4567@users.noreply.github.com>
2021-07-08 11:55:43 +0300

    Комментарии

--- tests.cpp
+++ tests.cpp
@@ -4,14 +4,14 @@
 
 TEST(Test, Test_foofail)
 {
-	std::optional<int> opt;
-	if (opt)
+	std::optional<int> opt; 
+	if (opt) //эту штуку сокращаю ниже
 	{
-		std::optional<int> b = cat(*opt);
+		std::optional<int> b = cat(*opt); //Если opt не False, то использовать в качестве аргумента функции cat
 	}
 
-	monadic_optional<int> opt2;
-	monadic_optional<int> t = opt2.and_then(cat);
+	monadic_optional<int> opt2; //Объект, у которого есть метод and_then
+	monadic_optional<int> t = opt2.and_then(cat); //Здесь ошибка, error_type для cat, и для cat не удается определить шаблон
 	
 	
 	

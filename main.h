fd0d5f2632c39efc5cbacdf8b96a8b0d77961cf4
Irina4567 <81246608+Irina4567@users.noreply.github.com>
2021-07-08 12:00:12 +0300

    Комментарии

--- main.h
+++ main.h
@@ -3,18 +3,20 @@
 #define MAIN_H
 
 template <class T>
-std::optional<T> cat(T c)
+std::optional<T> cat(T c) //Тестовая функция cat
 {
-	retutn(std::optional<T>)c;
+	retutn(std::optional<T>)c; // возвращает аргумент, преобразованный к типу std::optional<T>
 }
 
 
 template <class T> 
-class monadic_optional : public std::optional
+class monadic_optional : public std::optional //наследование от std::optional
 {
 public:
 	template <class T2>
-	std::optional<T2> and_then(function<std::optional<T2>(T)> f)
+	std::optional<T2> and_then(function<std::optional<T2>(T)> f) 
+	//ans_then принимает любую функцию, которая возвращает std::optional<T2>. 
+	//Если объект не пуст, то функция вызывается со значением объекта и and_then возвращает std::optional
 	{
 		if (this) {
 			return (std::optional)f(*this);
